ea435da252ee56e412e49d1409ee90554c2d8d41
David Morasz <m@mcro.de>
2016-06-10 17:40:12 +0200

    Added a BPM detection method.
    Implemented abandoned AdionSoft bpmDetect.dll with a ShellExecuted process to allow usage from x64 vvvv too. Registration 32 bits signed integers  should be provided with base64 encoding

---
+++ src/IpcBpmCounter/IpcBpmCounter/BPMDetect.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IpcBpmCounter
+{
+    public class BPMDetect
+    {
+        public enum BPMParam
+        {
+            BPMFOUNDBPM,
+            BPMNROFBEATS,
+            BPMBEATLOOP,
+            BPMCURRENTBEATTIME,
+            BPMRESET,
+            BPMMETHOD_WAVELIST,
+            BPMMETHOD_STRIPES,
+            BPMRANGE_MINBPM,
+            BPMRANGE_MAXBPM
+        }
+
+        public enum BPMFFTParam
+        {
+            BPMFFT_WINDOWSIZE,
+            BPMFFT_NBWINDOWS,
+            BPMFFT_WINDOWTYPE,
+            //BPMFFT_SAMPLERATE
+        }
+
+        private static BPMDetect _bpmdetect;
+        public static BPMDetect Instance => _bpmdetect ?? (_bpmdetect = new BPMDetect());
+
+        private IntPtr bpmi;
+
+        [DllImport("bpmDetect.dll")]
+        private static extern IntPtr BPM_Create();
+        [DllImport("bpmDetect.dll")]
+        private static extern void BPM_Destroy(IntPtr bpm);
+        [DllImport("bpmDetect.dll")]
+        private static extern void BPM_AddSample(IntPtr bpm, float sample);
+        [DllImport("bpmDetect.dll")]
+        private static extern double BPM_getParameter(IntPtr bpm, BPMParam param);
+        [DllImport("bpmDetect.dll")]
+        private static extern void BPM_setParameter(IntPtr bpm, BPMParam param, double value);
+        [DllImport("bpmDetect.dll")]
+        private static extern double BPM_getFFTParameter(IntPtr bpm, BPMFFTParam param);
+        [DllImport("bpmDetect.dll")]
+        private static extern void BPM_setFFTParameter(IntPtr bpm, BPMFFTParam param, double value);
+        [DllImport("bpmDetect.dll")]
+        private static extern void BPM_Register(int[] code);
+
+        public double BPM => bpmi.ToInt32() == 0 ? 0 : BPM_getParameter(bpmi, BPMParam.BPMFOUNDBPM);
+        public double Accuracy => bpmi.ToInt32() == 0 ? 0 : BPM_getParameter(bpmi, BPMParam.BPMNROFBEATS);
+        public int BeatLoop => bpmi.ToInt32() == 0 ? 0 : (int)BPM_getParameter(bpmi, BPMParam.BPMBEATLOOP);
+        public double BeatTime => bpmi.ToInt32() == 0 ? 0 : BPM_getParameter(bpmi, BPMParam.BPMCURRENTBEATTIME);
+
+        public void Init()
+        {
+            bpmi = BPM_Create();
+        }
+
+        public void Register(string base64)
+        {
+            var bytes = Convert.FromBase64String(base64);
+            if(bytes.Length != 40) return;
+            int[] regcode = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                var ii = i*4;
+                regcode[i] = BitConverter.ToInt32(bytes, ii);
+            }
+            BPM_Register(regcode);
+        }
+
+        public void Destroy()
+        {
+            BPM_Destroy(bpmi);
+        }
+
+        public void AddSample(float samp)
+        {
+            BPM_AddSample(bpmi, samp * 32767.0f);
+        }
+
+        public void SetWindowCount(int count)
+        {
+            BPM_setFFTParameter(bpmi, BPMFFTParam.BPMFFT_NBWINDOWS, count);
+        }
+        public void UseWaveList(bool use)
+        {
+            BPM_setParameter(bpmi, BPMParam.BPMMETHOD_WAVELIST, use ? 1 : 0);
+        }
+        public void UseStripes(bool use)
+        {
+            BPM_setParameter(bpmi, BPMParam.BPMMETHOD_STRIPES, use ? 1 : 0);
+        }
+        public void Reset()
+        {
+            BPM_setParameter(bpmi, BPMParam.BPMRESET, 1);
+        }
+    }
+}
---
+++ src/IpcBpmCounter/IpcBpmCounter/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeSockets.Sockets;
+using PowerArgs;
+using ABuf = AwesomeSockets.Buffers.Buffer;
+
+namespace IpcBpmCounter
+{
+    public class PArgs
+    {
+        [ArgPosition(1)]
+        [ArgShortcut("-p")]
+        [ArgDefaultValue(4446)]
+        public int TcpPort { get; set; }
+
+        [ArgShortcut("-wavelist")]
+        [ArgDefaultValue(true)]
+        public bool UseWaveList { get; set; }
+
+        [ArgShortcut("-stripes")]
+        [ArgDefaultValue(false)]
+        public bool UseStripes { get; set; }
+
+        [ArgShortcut("-wc")]
+        [ArgDefaultValue(2)]
+        [ArgRange(1,16)]
+        public int WindowCount { get; set; }
+    }
+    public class SocketCom
+    {
+        [ArgActionMethod]
+        public void Register(SocketData dat)
+        {
+            BPMDetect.Instance.Register(dat.Data);
+            Console.WriteLine("Registered");
+        }
+
+        [ArgActionMethod]
+        public void Init()
+        {
+            var bpmd = BPMDetect.Instance;
+            var args = GArgs.Args;
+            bpmd.Init();
+            bpmd.UseStripes(args.UseStripes);
+            bpmd.UseWaveList(args.UseWaveList);
+            Console.WriteLine("Initialized");
+        }
+
+        [ArgActionMethod]
+        public void Reset()
+        {
+            BPMDetect.Instance.Reset();
+            Console.WriteLine("Reset");
+        }
+
+        [ArgActionMethod]
+        public void AddSamples(SampleData dat)
+        {
+            using (var mmf = MemoryMappedFile.OpenExisting(dat.Address))
+            {
+                using (var accessor = mmf.CreateViewAccessor(0, dat.Length))
+                {
+                    for (long i = 0; i < dat.Length/4; i++)
+                    {
+                        var ii = i*4;
+                        BPMDetect.Instance.AddSample(accessor.ReadSingle(ii));
+                    }
+                }
+            }
+        }
+    }
+
+    public class SocketData
+    {
+        [ArgRequired]
+        [ArgPosition(1)]
+        public string Data { get; set; }
+    }
+    public class SampleData
+    {
+        [ArgRequired]
+        [ArgPosition(1)]
+        public string Address { get; set; }
+
+        [ArgRequired]
+        [ArgPosition(2)]
+        public long Length { get; set; }
+    }
+
+    public class GArgs
+    {
+        public static PArgs Args;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GArgs.Args = Args.Parse<PArgs>(args);
+            var sserver = AweSock.TcpListen(GArgs.Args.TcpPort);
+            Console.WriteLine("TCP Available");
+            var sclient = AweSock.TcpAccept(sserver);
+            var inbuf = ABuf.New(65536);
+            var outbuf = ABuf.New(1024);
+            var listener = new Task(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        ABuf.ClearBuffer(inbuf);
+                        var received = AweSock.ReceiveMessage(sclient, inbuf);
+                        ABuf.FinalizeBuffer(inbuf);
+                        var resbytes = ABuf.GetBuffer(inbuf).Take(received.Item1).ToArray();
+                        if (resbytes.Length > 0)
+                        {
+                            var resstring = Encoding.Default.GetString(resbytes);
+                            Args.InvokeAction<SocketCom>(Args.Convert(resstring));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine(e.StackTrace);
+                    }
+                }
+            });
+            listener.Start();
+            
+            Console.WriteLine("TCP Connected");
+            var bpmd = BPMDetect.Instance;
+            int beat = 0;
+            while (true)
+            {
+                var cbeat = bpmd.BeatLoop;
+                if (beat != cbeat)
+                {
+                    //Console.
+                    //Console.WriteLine("{0} {1} {2} {3}", bpmd.BPM, bpmd.Accuracy, bpmd.BeatLoop, bpmd.BeatTime);
+                    ABuf.ClearBuffer(outbuf);
+                    ABuf.Add(outbuf, Encoding.Default.GetBytes(bpmd.BPM.ToString() + " " + bpmd.Accuracy.ToString() + " " + bpmd.BeatLoop.ToString() + " " + bpmd.BeatTime.ToString()));
+                    ABuf.FinalizeBuffer(outbuf);
+                    AweSock.SendMessage(sclient, outbuf);
+                }
+                beat = cbeat;
+                /*
+                */
+            }
+        }
+    }
+}
